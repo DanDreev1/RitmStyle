b8aebfc2f0969a38e6affd6fba256a0f9fa6912b
DanDreev1 <75595828+DanDreev1@users.noreply.github.com>
2023-05-29 14:11:40 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,54 +1,54 @@
 // Отримуємо необхідні елементи з DOM
-let slider = document.querySelector('.slider'); // Отримуємо елемент слайдера з класом "slider"
-let slides = document.querySelectorAll('.slide'); // Отримуємо всі елементи слайдів з класом "slide"
-let activeSlide = document.querySelector('.slide--active'); // Отримуємо активний слайд з класом "slide--active"
+let slider = document.querySelector('.slider');
+let slides = document.querySelectorAll('.slide');
+let activeSlide = document.querySelector('.slide--active');
 
-let scrollSpeed = 300; // Встановлюємо швидкість прокручування слайдів (в мілісекундах)
-let isScrolling = false; // Флаг для визначення, чи відбувається прокручування
+let scrollSpeed = 300;
+let isScrolling = false;
 
 // Встановлюємо активний слайд
 function setActiveSlide(slideIndex) {
-  activeSlide.classList.remove('slide--active'); // Знімаємо клас "slide--active" з активного слайда
+  activeSlide.classList.remove('slide--active');
 
-  slides[slideIndex].classList.add('slide--active'); // Додаємо клас "slide--active" до обраного слайда
+  slides[slideIndex].classList.add('slide--active');
 
-  activeSlide = slides[slideIndex]; // Оновлюємо посилання на активний слайд
+  activeSlide = slides[slideIndex];
 
   // Переміщуємо слайдер, щоб активний слайд був по центру
-  let slideWidth = activeSlide.offsetWidth; // Ширина активного слайда
-  let sliderWidth = slider.offsetWidth; // Ширина слайдера
-  let sliderOffset = slider.getBoundingClientRect().left; // Відстань слайдера до лівого краю вікна
-  let slideOffset = activeSlide.getBoundingClientRect().left; // Відстань активного слайда до лівого краю вікна
-  let slideCenterOffset = slideOffset - sliderOffset + slideWidth / 2; // Відстань активного слайда до центру слайдера
-  let translateX = sliderWidth / 2 - slideCenterOffset; // Значення для зсуву слайдера
-  slider.style.transform = `translateX(${translateX}px)`; // Застосовуємо зсув до слайдера
+  let slideWidth = activeSlide.offsetWidth;
+  let sliderWidth = slider.offsetWidth;
+  let sliderOffset = slider.getBoundingClientRect().left;
+  let slideOffset = activeSlide.getBoundingClientRect().left; 
+  let slideCenterOffset = slideOffset - sliderOffset + slideWidth / 2; 
+  let translateX = sliderWidth / 2 - slideCenterOffset; 
+  slider.style.transform = `translateX(${translateX}px)`; 
 }
 
 // Функція для показу попереднього слайда
 function showPreviousSlide() {
-  if (isScrolling) return; // Якщо вже відбувається прокручування, вийти з функції
-  isScrolling = true; // Встановлюємо флаг прокручування
+  if (isScrolling) return; 
+  isScrolling = true; 
 
-  let currentIndex = Array.from(slides).indexOf(activeSlide); // Отримуємо індекс активного слайда
-  let previousIndex = (currentIndex - 1 + slides.length) % slides.length; // Обчислюємо індекс попереднього слайда (з врахуванням зацикленості)
-  setActiveSlide(previousIndex); // Встановлюємо попередній слайд як активний
+  let currentIndex = Array.from(slides).indexOf(activeSlide); 
+  let previousIndex = (currentIndex - 1 + slides.length) % slides.length; 
+  setActiveSlide(previousIndex); 
 
   setTimeout(function () {
-    isScrolling = false; // Після закінчення прокручування, знімаємо флаг прокручування
+    isScrolling = false; 
   }, scrollSpeed);
 }
 
 // Функція для показу наступного слайда
 function showNextSlide() {
-  if (isScrolling) return; // Якщо вже відбувається прокручування, вийти з функції
-  isScrolling = true; // Встановлюємо флаг прокручування
+  if (isScrolling) return; 
+  isScrolling = true; 
 
-  let currentIndex = Array.from(slides).indexOf(activeSlide); // Отримуємо індекс активного слайда
-  let nextIndex = (currentIndex + 1) % slides.length; // Обчислюємо індекс наступного слайда (з врахуванням зацикленості)
-  setActiveSlide(nextIndex); // Встановлюємо наступний слайд як активний
+  let currentIndex = Array.from(slides).indexOf(activeSlide); 
+  let nextIndex = (currentIndex + 1) % slides.length; 
+  setActiveSlide(nextIndex); 
 
   setTimeout(function () {
-    isScrolling = false; // Після закінчення прокручування, знімаємо флаг прокручування
+    isScrolling = false; 
   }, scrollSpeed);
 }
 
@@ -56,19 +56,19 @@ function showNextSlide() {
 slider.addEventListener('wheel', handleSliderScroll);
 
 function handleSliderScroll(event) {
-  event.preventDefault(); // Відміняємо стандартну поведінку скролу
-  event.stopPropagation(); // Зупиняємо подальше поширення події
+  event.preventDefault(); 
+  event.stopPropagation(); 
 
-  let delta = Math.sign(event.deltaY); // Визначаємо напрямок прокручування (1 - вниз, -1 - вгору)
+  let delta = Math.sign(event.deltaY); 
   if (delta > 0) {
-    showNextSlide(); // Показуємо наступний слайд при прокручуванні вниз
+    showNextSlide(); 
   } else {
-    showPreviousSlide(); // Показуємо попередній слайд при прокручуванні вгору
+    showPreviousSlide(); 
   }
 }
 
 // Задаємо активний слайд по центру після завантаження сторінки
 window.addEventListener('load', function () {
-  let initialSlideIndex = Array.from(slides).indexOf(activeSlide); // Отримуємо індекс активного слайда
-  setActiveSlide(initialSlideIndex); // Встановлюємо активний слайд по центру
+  let initialSlideIndex = Array.from(slides).indexOf(activeSlide); 
+  setActiveSlide(initialSlideIndex); 
 });
